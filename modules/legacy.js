398980823e57c244f73c0e93dea7dc6d1d32789f
spacemandev <devbharel@gmail.com>
2020-07-20 15:18:04 -0500

    added pack open settings

--- modules/legacy.js
+++ modules/legacy.js
@@ -1,5 +1,8 @@
+import { registerSettings } from './settings.js';
 import { FactionActorSheet } from "./faction-sheet.js";
 import { PowerItemSheet } from "./power-sheet.js";
+
+export const log = (...args) => console.log("Risk Legacy | " + args);
 
 Hooks.once("init", async () => {
   console.log("Initalizing Risk:Legacy Game System");
@@ -9,3 +12,13 @@ Hooks.once("init", async () => {
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("risk", PowerItemSheet, { makeDefault: false });
 });
+
+Hooks.on('ready', async () => {
+
+
+  //Parse the packs
+  log("Getting Risk Legacy Ready");
+  let gamepacks = jsyaml.safeLoad(await (await fetch('systems/risklegacy/assets/unlocks/unlocks.yaml')).text()).packs;
+  log(gamepacks); //list of objects
+  registerSettings(gamepacks); 
+}); 
---
+++ modules/settings.js
@@ -0,0 +1,73 @@
+export const system = "risklegacy"
+let packs = {}
+/**
+ * Creates a menu for opening new packs. Each Pack will register it's own setting.
+ * @param packs string[] list of game packs 
+*/
+export const registerSettings = (_packs) => {
+  console.log(_packs);
+  for(let p of Object.keys(_packs)){
+    game.settings.register(system, `${p}-opened`, {
+      scope: "world",
+      config: false,
+      type: Boolean,
+      default: false, 
+      onChange: _value => {
+        packs[p].opened = _value;
+      }
+    })
+    packs[p] = {
+      name: p, 
+      description: _packs[p].description,
+      opened: game.settings.get(system, `${p}-opened`)
+    }
+  }
+
+  game.settings.registerMenu(system, 'settingsMenu', {
+    name: "Packs",
+    label: "Open a New Pack", 
+    icon: "fas fa-book",
+    type: SettingsForm,
+    restricted: true,
+  })
+
+}
+
+class SettingsForm extends FormApplication{
+  constructor(object, options = {}){
+    super(object, options)
+  }
+
+  static get defaultOptions(){
+    return mergeObject(super.defaultOptions, {
+      id: system + ".openpacks",
+      title: "Open a New Pack",
+      template: "systems/risklegacy/templates/settings_packform.html",
+      classes: ["sheet"],
+      width: 800,
+      closeOnSubmit: true //need to close to refresh the packs openened "true/false"
+    })
+  }
+
+  getData(){
+    let data = {
+      packs: packs
+    }
+
+    Hooks.on("renderSettingsForm", async () => {
+      for(let pack of Object.keys(packs)){
+        $(`#${pack}`).on("click", async (evt) => {
+          game.settings.set(system, `${evt.target.value}-opened`, true);
+        })
+      }  
+    })
+
+    return data;
+  }
+
+  openPack(_packname){
+    console.log(_packname)
+  }
+
+  async _updateObject(evt, data) { }
+}
